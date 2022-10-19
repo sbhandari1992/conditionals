8ec4075aeeadc4bef76788c20600f0f436f37305
Sam Bhandari <sambhandari2021@yahooe.com>
2022-10-19 13:05:10 -0400

    practice age range

--- if_else.js
+++ if_else.js
@@ -1,13 +1,39 @@
-time = 22;
+// time = 22;
 
 
 
-if (time >= 6 & time < 12){
-    console.log('Good morning');
+// if (time >= 6 & time < 12){
+//     console.log('Good morning');
+// }
+// else if (time >= 12 & time < 18){
+//     console.log('Good afternoon');
+// }
+// else {
+//     console.log('Good evening');
+// }
+
+
+let age = 1;
+let ageCategories;
+
+if (age >=1 & age <= 5) {
+    ageCategories = 'Babys';
 }
-else if (time >= 12 & time < 18){
-    console.log('Good afternoon');
+else if (age >= 5 & age < 18) {
+    ageCategories = 'Kids';
+}
+else if (age >=18 & age < 25) {
+    ageCategories = 'Youth';
+}
+else if (age >=25 & age < 40) {
+    ageCategories = 'Adults';
+}
+else if (age >=40 & age <= 80 ) {
+    ageCategories = 'Siniors';
 }
 else {
-    console.log('Good evening');
+    let ageCategories = 'Expired'
 }
+console.log(ageCategories);
+
+
